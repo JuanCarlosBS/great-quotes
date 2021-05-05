3ba3b0dde24ed47b9aa4421adcd530ec8a1a6a56
juancarlosbs <juanrov_@hotmail.com>
2021-05-05 11:49:48 -0300

    feat: details page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,20 +1,31 @@
-import { Fragment } from 'react'
-import { useParams , Route } from 'react-router-dom'
+import { Fragment } from 'react';
+import { useParams, Route } from 'react-router-dom';
 
-import Comments from '../components/comments/Comments'
- 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
 const QuoteDetail = () => {
-    const params = useParams()
+  const params = useParams();
 
-    return (
-        <Fragment>
-            <h1>Quote Detail Page</h1>
-            <p>{params.quoteId}</p>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
-                <Comments />
-            </Route>
-        </ Fragment>
-    )
-}
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-export default QuoteDetail
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
